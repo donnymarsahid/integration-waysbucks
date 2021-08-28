fbbb9c8b24f1525ecef0821a30faf0a256a198be
unknown <donnymrshd.94@gmail.com>
2021-08-28 20:42:24 +0700

    users api

---
+++ src/controllers/user.js
@@ -0,0 +1,46 @@
+const { user } = require('../../models');
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await user.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt', 'status', 'password'],
+      },
+    });
+    res.send({
+      status: 'success',
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await user.destroy({
+      where: {
+        id,
+      },
+    });
+    res.send({
+      status: 'success',
+      data: {
+        id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { login, register } = require('../controllers/auth');
+const { getUsers, deleteUser } = require('../controllers/user');
 
 router.post('/login', login);
 router.post('/register', register);
+
+router.get('/users', getUsers);
+router.delete('/user/:id', deleteUser);
 
 module.exports = router;
